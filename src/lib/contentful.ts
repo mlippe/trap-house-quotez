b7f1133e78acf4455be007fbf5a185ada9676ff6
Thomas May <thomas.may@produktmacher.com>
2023-01-20 13:49:38 +0100

    Integrate Contentful

---
+++ src/lib/contentful.ts
@@ -0,0 +1,14 @@
+import { createClient } from 'contentful'
+import type { EntryCollection } from 'contentful'
+
+export const contentfulClient = createClient({
+  space: import.meta.env.CONTENTFUL_SPACE_ID,
+  environment: import.meta.env.CONTENTFUL_ENVIRONMENT,
+  accessToken: import.meta.env.DEV
+    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
+    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
+  host: import.meta.env.DEV ? 'preview.contentful.com' : 'cdn.contentful.com'
+})
+
+export const extractFields = <T>(entries: EntryCollection<T>) =>
+  entries.items.map(({ fields }) => fields)
