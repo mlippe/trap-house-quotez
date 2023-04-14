ecb15acaf392b76af9c7d57bb8047110f137cd3a
Thomas May <thomas.may@produktmacher.com>
2023-04-14 15:07:39 +0200

    Update helloWorld function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,8 +3,14 @@ import * as cors from 'cors'
 
 const handler = cors({ origin: true })
 
-export const helloWorld = functions.https.onRequest((_, response) => {
-  handler(_, response, () => {
-    response.send({ message: 'Sheeesh from ze boy 🔪' })
+export const helloWorld = functions.https.onRequest((request, response) => {
+  handler(request, response, () => {
+    const data = {
+      title: request.body.fields.title['en-US'],
+      startTimestamp: request.body.fields.startTimestamp['en-US'],
+      endTimestamp: request.body.fields.endTimestamp['en-US']
+    }
+    functions.logger.log(data)
+    response.send({ message: 'Sheeesh from ze boy 🔪', data })
   })
 })
